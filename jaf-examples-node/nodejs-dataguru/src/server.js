a5d8815d5afe04a0fa48547fc14d353205064d42
liaozhicheng.cn@163.com <liaozhicheng.cn@163.com>
2016-06-26 21:09:51 +0800

    add nodejs lesson4

--- jaf-examples-node/nodejs-dataguru/src/server.js
+++ jaf-examples-node/nodejs-dataguru/src/server.js
@@ -10,6 +10,11 @@ import debug from 'debug';
 
 const $ = global.$ = new ProjectCore();
 
+// 创建 debug
+$.debug = function(name) {
+  return debug('nodejsDataguru:' + name);
+}
+
 // 初始化 model 模块
 $.model = {};
 
@@ -18,16 +23,17 @@ $.init.add((done) => {
   $.config.load(path.resolve(__dirname, 'config.js'));
   const env = process.env.NODE_ENV || null;
   if(env) {
-    $.config.load(path.resolve(__dirname, '../config', env.trim() + ".js"));
+    try {
+      $.config.load(path.resolve(__dirname, '../config', env.trim() + ".js"));
+    } catch(err) {
+      console.log(err.stack);
+    }
   }
   $.env = env;
   done();
 });
 
-// 创建 debug
-$.debug = function(name) {
-  return debug('nodejsDataguru:' + name);
-}
+
 
 const serverDebug = $.debug('server');
 
@@ -41,27 +47,18 @@ $.init.load(path.resolve(__dirname, 'methods'));
 
 // 初始化 express
 $.init.load(path.resolve(__dirname, 'init', 'express.js'));
+// 初始化中间件
+$.init.load(path.resolve(__dirname, 'middlewares'));
 // 加载 route 模块
 $.init.load(path.resolve(__dirname, 'routes'));
 
 // 初始化
 $.init((err) => {
   if(err) {
-    // console.log(err);
-
     serverDebug(err);
     process.exit(-1);
   } else {
     serverDebug('inited [evn=%s]', $.env);
-    // console.log('inited [evn=%s]', $.env);
   }
-
-  // const item = new $.model.User({
-  //   name: 'test21',
-  //   password: '123456',
-  //   nickname: '测试用户001'
-  // });
-  // item.save(console.log);
-  // require('./test')
 
 });
