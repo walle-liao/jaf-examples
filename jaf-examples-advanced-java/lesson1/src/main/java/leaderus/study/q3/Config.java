9dfea2ba8a12896ccbc1a2a85236d1cc6ce20f59
liaozhicheng.cn@163.com <liaozhicheng.cn@163.com>
2016-08-12 00:47:26 +0800

    add advanced java

---
+++ jaf-examples-advanced-java/lesson1/src/main/java/leaderus/study/q3/Config.java
@@ -0,0 +1,16 @@
+package leaderus.study.q3;
+
+/**
+ * TODO
+ * 
+ * @author liaozhicheng
+ * @date 2016年8月11日
+ * @since 1.0
+ */
+class Config {
+
+	static final int TOTAL_ROWS = 5000000;
+	static final String SPLIT_CHAR = ",";
+	static final String DB_FILE_NAME = "d:/temp/persondb.txt";
+	
+}
