24b3fc1b75f466aa0659e1798d3c051720bb8a92
liaozhicheng.cn@163.com <liaozhicheng.cn@163.com>
2016-08-28 09:08:54 +0800

    add project j2se

---
+++ jaf-examples-j2se/src/main/java/com/jaf/examples/j2se/java/util/ListDeleteItem.java
@@ -0,0 +1,15 @@
+package com.jaf.examples.j2se.java.util;
+
+
+/**
+ * list 中正确的迭代和删除元素
+ * 
+ * @author liaozhicheng.cn@163.com
+ * @date 2016年8月28日
+ * @since 1.0
+ */
+public class ListDeleteItem {
+	
+	
+	
+}
