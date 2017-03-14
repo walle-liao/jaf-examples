b378d34fbf97923654bfa0de1b9cf3175c0a8e4c
walle <liaozhicheng.cn@163.com>
2017-03-14 21:41:36 +0800

    modify

--- jaf-examples-nio/src/main/java/com/jaf/examples/telnet/NioTelnetServer.java
+++ jaf-examples-nio/src/main/java/com/jaf/examples/telnet/NioTelnetServer.java
@@ -13,6 +13,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
+ * nio + thread pool
  * Created by walle on 2017/3/12.
  */
 public class NioTelnetServer {
@@ -20,7 +21,6 @@ public class NioTelnetServer {
     public static void main(String[] args) throws IOException {
         NioTelnetServer.start();
     }
-
 
     public static void start() throws IOException {
         ExecutorService executorService = Executors.newFixedThreadPool(10);
@@ -37,23 +37,22 @@ public class NioTelnetServer {
             int selectNum = selector.select();
             if(selectNum == 0)
                 continue;
-
             Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
             while(keyIterator.hasNext()) {
                 SelectionKey key = keyIterator.next();
                 if(key.isAcceptable()) {
-                    System.out.println(key);
-
                     ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
                     SocketChannel socketChannel = serverChannel.accept();
                     socketChannel.configureBlocking(false);
-                    socketChannel.register(selector, SelectionKey.OP_READ);
-                    socketChannel.write(ByteBuffer.wrap("welcome telnet server ... \r\n".getBytes(Charset.forName("UTF-8"))));
+                    socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                    socketChannel.write(ByteBuffer.wrap("welcome telnet server ... \r\n".getBytes()));
                 } else if(key.isReadable()) {
                     System.out.println(key);
                     executorService.execute(new ReadEventHandler(key));
-//                    key.cancel();
                     key.interestOps(key.interestOps()&(~SelectionKey.OP_READ));
+                } else if(key.isWritable()) {
+                    System.out.println(key);
+                    executorService.execute(new WriteEventHandler(key));
                 }
                 keyIterator.remove();
             }
@@ -75,12 +74,10 @@ public class NioTelnetServer {
                 ByteBuffer buffer = ByteBuffer.allocate(100);
                 buffer.put("\r\nFollow you: ".getBytes(Charset.forName("UTF-8")));
                 int size = socketChannel.read(buffer);
-                System.out.println("size:");
-                System.out.println(size);
                 buffer.put("\r\n".getBytes(Charset.forName("UTF-8")));
                 buffer.flip();
                 socketChannel.write(buffer);
-                if(size == -1) {
+                if(size == -1) {  // 链接关闭
                     socketChannel.close();
                 } else {
                     selectionKey.interestOps(selectionKey.interestOps() | SelectionKey.OP_READ);
@@ -89,6 +86,22 @@ public class NioTelnetServer {
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
+        }
+    }
+
+    private static class WriteEventHandler implements Runnable {
+
+        private final SelectionKey selectionKey;
+
+        private WriteEventHandler(SelectionKey selectionKey) {
+            this.selectionKey = selectionKey;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("receive write event");
+            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+            selectionKey.attachment();
         }
     }
 
