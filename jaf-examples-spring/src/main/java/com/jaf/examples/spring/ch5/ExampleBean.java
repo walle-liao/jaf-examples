055c8a79287d1992ddc15d100aceb5dab3df95b3
walle <liaozhicheng.cn@163.com>
2017-04-17 07:17:35 +0800

    add spring framework

---
+++ jaf-examples-spring/src/main/java/com/jaf/examples/spring/ch5/ExampleBean.java
@@ -0,0 +1,18 @@
+package com.jaf.examples.spring.ch5;
+
+/**
+ * Created by walle on 2017/4/8.
+ */
+public class ExampleBean {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
