44de15ef7d453ee0015af761b283b0bbc7a26973
walle <liaozhicheng.cn@163.com>
2017-04-20 07:17:21 +0800

    modify

---
+++ jaf-examples-spring/src/main/java/com/jaf/examples/spring/ch7/dynamicproxy/cglib/CglibProxyService.java
@@ -0,0 +1,7 @@
+package com.jaf.examples.spring.ch7.dynamicproxy.cglib;
+
+/**
+ * Created by walle on 2017/4/20.
+ */
+public class CglibProxyService {
+}
---
+++ jaf-examples-spring/src/main/java/com/jaf/examples/spring/ch7/dynamicproxy/cglib/RealService.java
@@ -0,0 +1,12 @@
+package com.jaf.examples.spring.ch7.dynamicproxy.cglib;
+
+/**
+ * Created by walle on 2017/4/20.
+ */
+public class RealService {
+
+    public void sayHello() {
+        System.out.println("hello world");
+    }
+
+}
